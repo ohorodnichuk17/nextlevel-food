aaf9b4c6edfc9f543e0218a15462c7bd4a1e5258
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-09-18 14:27:18 +0300

    stored server actions in separate file

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,21 +1,11 @@
+'use client';
+
 import classes from './page.module.css';
 import ImagePicker from "@/app/meals/image-picker";
+import {shareMeal} from "@/lib/actions";
 
 export default function ShareMealPage() {
-    async function shareMeal(formData) {
-        'use server';
 
-        const meal = {
-            title: formData.get('title'),
-            summary: formData.get('summary'),
-            instructions: formData.get('instructions'),
-            image: formData.get('image'),
-            creator: formData.get('name'),
-            creator_email: formData.get('email')
-        };
-
-        console.log(meal);
-    }
 
     return (
         <>
