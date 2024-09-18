a334639eb8cb30c0daeae4622f2640d9dac06058
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-09-18 10:09:36 +0300

    feat: custom image picker input component

---
+++ app/meals/image-picker.js
@@ -0,0 +1,34 @@
+'use client';
+
+import classes from './image-picker.module.css'
+import {useRef} from "react";
+
+export default function ImagePicker({label, name}) {
+    const imageInput = useRef();
+
+    function handlePickClick() {
+        imageInput.current.click();
+    }
+
+    return (
+        <div className={classes.picker}>
+            <label htmlFor={name}>{label}</label>
+            <div className={classes.controls}>
+                <input
+                    className={classes.input}
+                    type="file"
+                    id={name}
+                    accept="image/png, image/jpeg"
+                    name={name}
+                    ref={imageInput}
+                />
+                <button
+                    onClick={handlePickClick}
+                    className={classes.button} type="button"
+                >
+                    Pick an Image
+                </button>
+            </div>
+        </div>
+    );
+}
